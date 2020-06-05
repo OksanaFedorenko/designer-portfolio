aa6d59dfd65889afcc694f814a82891a540bfb0e
Oksana Fedorenko <oksanafedorenko2016@gmail.com>
2020-06-05 22:24:51 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,40 @@
+  // Progress bar
+  
+  var progressId = [
+    "nanoprogress1",
+    "nanoprogress2",
+    "nanoprogress3",
+    "nanoprogress4",
+  ];
+
+  var percentage = [80, 65, 50, 30];
+
+  for (let i=0; i < progressId.length; i++) {
+    var options = {
+      classname: "my-class",
+      id: "my-id",
+      target: document.getElementById(progressId[i]),
+    };
+
+    console.log(progressId[i]);
+
+    var nanobar = new Nanobar(options);
+
+    // move bar
+    nanobar.go(percentage[i]);
+  };
+
+
+
+/*var options = {
+  classname: 'my-class',
+  id: 'my-id',
+  target: document.getElementById('nanoprogress1')
+};
+
+var nanobar = new Nanobar( options );
+
+// move bar
+nanobar.go( 30 ); // size bar 30%*/
+
+
